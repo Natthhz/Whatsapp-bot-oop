c3bb1030e9c43ad07f26a945abbc752431cabca4
Andi Raif <andiraif10@gmail.com>
2025-09-26 16:18:15 +0700

    Update database.js

--- node_backend/src/bots/config/database.js
+++ node_backend/src/bots/config/database.js
@@ -6,7 +6,7 @@ dotenv.config();
 export const databaseConfig = {
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'Andi naruto123',
+    password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME || 'whatsapp_bot3',
     port: parseInt(process.env.DB_PORT) || 3306,
     waitForConnections: true,
